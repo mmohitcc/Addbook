09c3ad3e060a624c3925d61393f4a43acb5fe80c
mmohitcc <mohitchaudhary@Mohits-MacBook-Pro.local>
2017-05-05 17:05:51 -0600

    updated index page
    index page now shows all address books, and each address book has links
    to view the address book.
    
    new address book button

--- Addressbook/app/controllers/addbooks_controller.rb
+++ Addressbook/app/controllers/addbooks_controller.rb
@@ -1,7 +1,9 @@
 class AddbooksController < ApplicationController
+	before_action :find_message, only: [:show, :edit, :update, :destroy]
+
 
 	def index
-
+		@addbooks = Addbook.all.order("created_at DESC")
 	end
 
 	def new
@@ -17,6 +19,9 @@ class AddbooksController < ApplicationController
 		end
 	end
 
+	def show
+	end
+
 
 
 	private
@@ -24,5 +29,13 @@ class AddbooksController < ApplicationController
 	def addbook_params
 		params.require(:addbook).permit(:title, :description)
 	end
+
+	
+	def find_message
+			@addbook = Addbook.find(params[:id])
+	end
+
+
+
 
 end
